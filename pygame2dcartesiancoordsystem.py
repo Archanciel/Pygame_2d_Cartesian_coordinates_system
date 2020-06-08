be4523e9845b9f18dea0a6af8acc9845119cd3cd
Archanciel <jp.schnyder@gmail.com>
2020-06-08 16:13:19 +0200

    refactoring for unit test

--- pygame2dcartesiancoordsystem.py
+++ pygame2dcartesiancoordsystem.py
@@ -22,26 +22,12 @@ class Pygame2dCartesianCoordSystem():
 		
 		# computing how many pixels correspond to delta y
 		self.yStep = math.floor(yLength / (yRange[0] - yRange[1]))
-		
-		if xRange[0] < 0:
-			if xRange[1] < 0:
-				self.xAxisCoordStart = (origin[0] - xLength, origin[1])
-			else:
-				self.xAxisCoordStart = (origin[0] - abs(xRange[0] * self.xStep), origin[1])
-		else:
-			self.xAxisCoordStart = (origin[0], origin[1])
 
-		if yRange[0] > 0:
-			if yRange[1] > 0:
-				self.yAxisCoordStart = (origin[0], origin[1] - yLength)
-			else:
-				self.yAxisCoordStart = (origin[0], origin[1] - yRange[0] * self.yStep)
-		else:
-			self.yAxisCoordStart = (origin[0], origin[1])
-			 
-		self.xAxisCoordEnd = (self.xAxisCoordStart[0] + xLength, self.xAxisCoordStart[1])
-		self.yAxisCoordEnd = (self.yAxisCoordStart[0], self.yAxisCoordStart[1] + yLength)
-		self.axesColor = BLACK
+		self.xAxisCoordStart, self.xAxisCoordEnd = self.computeXAxisCoordinates(origin, xLength, xRange)
+
+		self.yAxisCoordStart, self.yAxisCoordEnd = self.computeYAxisCoordinates(origin, yLength, yRange)
+
+		self.axesColor = color
 		
 		# computing x axis arrow coord
 		xArrowPoint1 = (self.xAxisCoordEnd[0] - ARROW_LENGTH, self.xAxisCoordEnd[1] - ARROW_BASE / 2)
@@ -65,7 +51,55 @@ class Pygame2dCartesianCoordSystem():
 		self.yLabelText = font.render(self.yLabel, True, self.color)
 		self.yLabelHorizontalShift = font.size(yLabel)[0] / 2
 		self.originText = font.render("O", True, self.color)
-		
+
+	def computeXAxisCoordinates(self, origin, xAxisLength, xAxisValueRange):
+		"""
+		This method computes the X axis line start and end coordinates expressed in pixels.
+		The returned tuples will be used by the Pygame.draw.line method to draw the X axis.
+
+		:param origin: Cartesian axes system origin coordinate tuple in pixels. Its position
+			   in the Pygame Surface.
+		:param xAxisLength:
+		:param xAxisValueRange: tuple containing the minimal and maximal x value
+
+		:return: xAxisCoordStart and xAxisCoordEnd tuples
+		"""
+		if xAxisValueRange[0] < 0:
+			if xAxisValueRange[1] < 0:
+				xAxisCoordStart = (origin[0] - xAxisLength, origin[1])
+			else:
+				xAxisCoordStart = (origin[0] - abs(xAxisValueRange[0] * self.xStep), origin[1])
+		else:
+			xAxisCoordStart = (origin[0], origin[1])
+
+		xAxisCoordEnd = (xAxisCoordStart[0] + xAxisLength, xAxisCoordStart[1])
+
+		return xAxisCoordStart, xAxisCoordEnd
+
+	def computeYAxisCoordinates(self, origin, yLength, yRange):
+		"""
+		This method computes the Y axis line start and end coordinates expressed in pixels.
+		The returned tuples will be used by the Pygame.draw.line method to draw the X axis.
+
+		:param origin: Cartesian axes system origin coordinate tuple in pixels. Its position
+			   in the Pygame Surface.
+		:param xAxisLength:
+		:param xAxisValueRange: tuple containing the minimal and maximal y value
+
+		:return: yAxisCoordStart and yAxisCoordEnd tuples
+		"""
+		if yRange[0] > 0:
+			if yRange[1] > 0:
+				yAxisCoordStart = (origin[0], origin[1] - yLength)
+			else:
+				yAxisCoordStart = (origin[0], origin[1] - yRange[0] * self.yStep)
+		else:
+			yAxisCoordStart = (origin[0], origin[1])
+
+		yAxisCoordEnd = (yAxisCoordStart[0], yAxisCoordStart[1] + yLength)
+
+		return yAxisCoordStart, yAxisCoordEnd
+
 	def draw(self):
 		# drawing x axis
 		pg.draw.line(self.screen, self.axesColor, self.xAxisCoordStart, self.xAxisCoordEnd, self.thickness)
