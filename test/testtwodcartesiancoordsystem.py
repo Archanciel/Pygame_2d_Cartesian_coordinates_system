e31c21b3699f5d3bb4e2043f019a60e675999bd7
Archanciel <jp.schnyder@gmail.com>
2020-06-10 16:37:26 +0200

    tests ok

--- test/testtwodcartesiancoordsystem.py
+++ test/testtwodcartesiancoordsystem.py
@@ -65,5 +65,119 @@ class TestTwoDCartesianCoordSystem(unittest.TestCase):
 		self.assertEqual((300, 570), ccs.yAxisCoordStart)
 		self.assertEqual((300, 770), ccs.yAxisCoordEnd)
 
+	def testComputeXAxisArrowCoordinates_X_neg_X_pos(self):
+		origin = (80, 400)
+		ccs = TwoDCartesianCoordSystem(origin=origin, xLength=200, yLength=300, xRange=(-5, 15),
+									   yRange=(50, -3), titleLst=["xR=(-5,15)", "yR=(50,-3)"])
+
+		self.assertEqual(((220, 385.0), (260, 400), (220, 415.0)), ccs.computeXAxisArrowCoordinates(origin, (60, 400), (260, 400)))
+
+	def testComputeXAxisArrowCoordinates_X_zero_X_pos(self):
+		origin = (500, 530)
+		ccs = TwoDCartesianCoordSystem(origin=origin, xLength=200, yLength=300, xRange=(0, 15),
+										   yRange=(50, 3), titleLst=["xR=(0,15)", "yR=(50,3)"])
+
+		self.assertEqual(((660, 515.0), (700, 530), (660, 545.0)), ccs.computeXAxisArrowCoordinates(origin, (500, 530), (700, 530)))
+
+	def testComputeXAxisArrowCoordinates_X_pos_X_pos(self):
+		origin = (720, 530)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=400, xRange=(25, 55),
+										   yRange=(50, 3), titleLst=["xR=(25,55)", "yR=(50,3)"])
+
+		self.assertEqual(((880, 515.0), (920, 530), (880, 545.0)), ccs.computeXAxisArrowCoordinates(origin, (720, 530), (920, 530)))
+
+	def testComputeXAxisArrowCoordinates_X_neg_X_neg(self):
+		origin = (1145, 550)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=400,
+										   xRange=(-55, -5), yRange=(50, 0), titleLst=["xR=(-55,-5)", "yR=(50,0)"])
+
+		self.assertEqual(((985, 535.0), (945, 550), (985, 565.0)), ccs.computeXAxisArrowCoordinates(origin, (945, 550), (1145, 550)))
+
+	def testComputeYAxisArrowCoordinates_Y_pos_Y_neg(self):
+		origin = (300, 255)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=300, xRange=(-5, 15),
+									   yRange=(30, -40), xLabel='Time', yLabel='Speed',
+									   titleLst=["xR=(-5,15)", "yR=(30,-40)"])
+
+		self.assertEqual(((285.0, 265), (300, 225), (315.0, 265)), ccs.computeYAxisArrowCoordinates(origin, (300, 225), (300, 525)))
+
+	def testComputeYAxisArrowCoordinates_Y_pos_Y_zero(self):
+		origin = (1145, 550)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=400,
+										   xRange=(-55, -5), yRange=(50, 0), titleLst=["xR=(-55,-5)", "yR=(50,0)"])
+
+		self.assertEqual(((1130.0, 540), (1145, 500), (1160.0, 540)), ccs.computeYAxisArrowCoordinates(origin, (1145, 500), (1145, 550)))
+
+	def testComputeYAxisArrowCoordinates_Y_pos_Y_pos(self):
+		origin = (500, 530)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=400, xRange=(0, 15),
+										   yRange=(50, 3), titleLst=["xR=(0,15)", "yR=(50,3)"])
+
+		self.assertEqual(((485.0, 520), (500, 480), (515.0, 520)), ccs.computeYAxisArrowCoordinates(origin, (500, 480), (500, 530)))
+
+	def testComputeYAxisArrowCoordinates_Y_neg_Y_neg(self):
+		origin = (300, 570)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=200, xRange=(0, 55),
+										   yRange=(-50, -3), titleLst=["xR=(0,55)", "yR=(-50,-3)"])
+
+		self.assertEqual(((285.0, 580), (300, 620), (315.0, 580)), ccs.computeYAxisArrowCoordinates(origin, (300, 570), (300, 620)))
+
+	def testComputeXAxisLabelXCoordAndDirection_X_neg_X_pos(self):
+		origin = (80, 400)
+		ccs = TwoDCartesianCoordSystem(origin=origin, xLength=200, yLength=300, xRange=(-5, 15),
+									   yRange=(50, -3), titleLst=["xR=(-5,15)", "yR=(50,-3)"])
+
+		self.assertEqual((260, 1), ccs.computeXAxisLabelXCoordAndDirection(origin, (60, 400), (260, 400)))
+
+	def testComputeXAxisLabelXCoordAndDirection_X_zero_X_pos(self):
+		origin = (500, 530)
+		ccs = TwoDCartesianCoordSystem(origin=origin, xLength=200, yLength=300, xRange=(0, 15),
+										   yRange=(50, 3), titleLst=["xR=(0,15)", "yR=(50,3)"])
+
+		self.assertEqual((700, 1), ccs.computeXAxisLabelXCoordAndDirection(origin, (500, 530), (700, 530)))
+
+	def testComputeXAxisLabelXCoordAndDirection_X_pos_X_pos(self):
+		origin = (720, 530)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=400, xRange=(25, 55),
+										   yRange=(50, 3), titleLst=["xR=(25,55)", "yR=(50,3)"])
+
+		self.assertEqual((920, 1), ccs.computeXAxisLabelXCoordAndDirection(origin, (720, 530), (920, 530)))
+
+	def testComputeXAxisLabelXCoordAndDirection_X_neg_X_neg(self):
+		origin = (1145, 550)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=400,
+										   xRange=(-55, -5), yRange=(50, 0), titleLst=["xR=(-55,-5)", "yR=(50,0)"])
+
+		self.assertEqual((945, -1), ccs.computeXAxisLabelXCoordAndDirection(origin, (945, 550), (1145, 550)))
+
+	def testComputeYAxisLabelXCoordAndDirection_Y_pos_Y_neg(self):
+		origin = (300, 255)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=300, xRange=(-5, 15),
+									   yRange=(30, -40), xLabel='Time', yLabel='Speed',
+									   titleLst=["xR=(-5,15)", "yR=(30,-40)"])
+
+		self.assertEqual((300, 1), ccs.computeYAxisLabelXCoordAndDirection(origin, (300, 225)))
+
+	def testComputeYAxisLabelXCoordAndDirection_Y_pos_Y_zero(self):
+		origin = (1145, 550)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=400,
+										   xRange=(-55, -5), yRange=(50, 0), titleLst=["xR=(-55,-5)", "yR=(50,0)"])
+
+		self.assertEqual((1145, 1), ccs.computeYAxisLabelXCoordAndDirection(origin, (1145, 500)))
+
+	def testComputeYAxisLabelXCoordAndDirection_Y_pos_Y_pos(self):
+		origin = (500, 530)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=400, xRange=(0, 15),
+										   yRange=(50, 3), titleLst=["xR=(0,15)", "yR=(50,3)"])
+
+		self.assertEqual((500, 1), ccs.computeYAxisLabelXCoordAndDirection(origin, (500, 480)))
+
+	def testComputeYAxisLabelXCoordAndDirection_Y_neg_Y_neg(self):
+		origin = (300, 570)
+		ccs = TwoDCartesianCoordSystem(origin, xLength=200, yLength=200, xRange=(0, 55),
+										   yRange=(-50, -3), titleLst=["xR=(0,55)", "yR=(-50,-3)"])
+
+		self.assertEqual((300, -1), ccs.computeYAxisLabelXCoordAndDirection(origin, (300, 570)))
+
 if __name__ == '__main__':
 	unittest.main()
