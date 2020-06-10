f055c9561773de8fcd71c21821ec758c7bee3ce8
Archanciel <jp.schnyder@gmail.com>
2020-06-10 19:41:44 +0200

    modified systems on Windows

--- draw_cartesian_axes.py
+++ draw_cartesian_axes.py
@@ -87,10 +87,10 @@ else:
 		PygameTwoDCartesianCoordSystem(screen=screen, origin=(300, 255), xLength=200, yLength=300, xRange=(-5, 15),
 									   yRange=(30, -40), xLabel='Time', yLabel='Speed', titleLst=["xR=(-5,15)","yR=(30,-40)"]))
 	cartesianAxesLst.append(
-		PygameTwoDCartesianCoordSystem(screen=screen, origin=(500, 530), xLength=200, yLength=400, xRange=(0, 15),
+		PygameTwoDCartesianCoordSystem(screen=screen, origin=(500, 400), xLength=200, yLength=270, xRange=(0, 15),
 									   yRange=(50, 3), titleLst=["xR=(0,15)","yR=(50,3)"]))
 	cartesianAxesLst.append(
-		PygameTwoDCartesianCoordSystem(screen=screen, origin=(720, 530), xLength=200, yLength=400, xRange=(25, 55),
+		PygameTwoDCartesianCoordSystem(screen=screen, origin=(720, 400), xLength=200, yLength=270, xRange=(25, 55),
 									   yRange=(50, 3), titleLst=["xR=(25,55)","yR=(50,3)"]))
 	cartesianAxesLst.append(
 		PygameTwoDCartesianCoordSystem(screen=screen, origin=(1145, 550), xLength=200, yLength=400, xRange=(-55, -5),
@@ -99,7 +99,7 @@ else:
 		PygameTwoDCartesianCoordSystem(screen=screen, origin=(300, 555), xLength=200, yLength=200, xRange=(0, 55),
 									   yRange=(-50, -3), yLabel="Depth", titleLst=["xR=(0,55)","yR=(-50,-3)"]))
 	cartesianAxesLst.append(
-		PygameTwoDCartesianCoordSystem(screen=screen, origin=(720, 555), xLength=200, yLength=400, xRange=(-55, -5),
+		PygameTwoDCartesianCoordSystem(screen=screen, origin=(720, 555), xLength=200, yLength=200, xRange=(-55, -5),
 									   yRange=(-50, -3), xLabel="Loss", yLabel="Depth", titleLst=["xR=(-55,-5)","yR=(-50,-3)"]))
 
 while running:
