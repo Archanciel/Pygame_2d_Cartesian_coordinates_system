b1621823289eb09a0034f5d35936a24b57539324
Archanciel <jp.schnyder@gmail.com>
2020-06-10 18:33:32 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,28 @@
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (2, 255, 255)
+MAGENTA = (255, 0, 255)
+ORANGE = (255, 165, 0)
+
+# frame rate per second
+FPS = 10
+
+FONT_TITLE_SIZE = 32
+FONT_SIZE = 28
+
+ORIGIN_LABEL = "O"
+
+# axis arrow dimensions
+ARROW_BASE = 30
+ARROW_LENGTH = 40
+
+# Cartesian coordinates system quadrants
+QUADRANT_1 = 1
+QUADRANT_2 = 2
+QUADRANT_3 = 3
+QUADRANT_4 = 4
